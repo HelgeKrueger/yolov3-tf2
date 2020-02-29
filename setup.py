c4b4fa5fcc726504f43667ea1423942520b5bf2c
Helge <helge.krueger@gmail.com>
2020-02-29 20:19:48 +0100

    Add requirements to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup
 
+with open('requirements-gpu.txt') as f:
+    requirements = f.read().splitlines()
+    
+requirements = [r for r in requirements if r not in ['', '-e .']]
+
 setup(name='yolov3_tf2',
       version='0.1',
       url='https://github.com/zzh8829/yolov3-tf2',
       author='Zihao Zhang',
       author_email='zzh8829@gmail.com',
-      packages=['yolov3_tf2'])
+      packages=['yolov3_tf2'],
+      install_requires=requirements)
