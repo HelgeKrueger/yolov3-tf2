e557a1f05c051ef26fd8db35f151e291b042ce59
Helge <helge.krueger@gmail.com>
2020-03-03 20:06:30 +0100

    add code for yolo

--- yolov3_tf2/__init__.py
+++ yolov3_tf2/__init__.py
@@ -1,14 +1,28 @@
-from yolov3_tf2.models import YoloV3Tiny
+from yolov3_tf2.models import YoloV3Tiny, YoloV3
 from yolov3_tf2.dataset import transform_images
 
 from absl import flags
 
-
-def create_yolo_tiny():
+def handle_flags(**kwargs):
     flags.FLAGS(['program_name'])
+
+    if kwargs is not None:
+        for key, value in kwargs.items():
+            setattr(flags.FLAGS, key, value)
+
+def create_yolo_tiny(**kwargs):
+    handle_flags(**kwargs)
 
     yolo = YoloV3Tiny(classes=80)
     yolo.load_weights('data/yolov3-tiny.tf').expect_partial()
+
+    return yolo
+
+def create_yolo(**kwargs):
+    handle_flags(**kwargs)
+
+    yolo = YoloV3(classes=80)
+    yolo.load_weights('data/yolov3.tf').expect_partial()
 
     return yolo
 
